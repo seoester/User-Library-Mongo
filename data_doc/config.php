0311b1f8ac458f724de8a1435f7fdedd8122d97f
Seoester <seoester@googlemail.com>
2013-02-08 00:03:23 +0100

    Created new configuration system

---
+++ data_doc/config.php
@@ -0,0 +1,31 @@
+<?php
+
+$configArray = array(
+	"DBServer" => "localhost",
+	"DBPort" => "27017",
+	"DBDatabase" => "foodorder",
+	"DBPrefix" => "",
+
+	"loginEnabled" => true,
+	"registerEnabled" => true,
+	"needApproval" => true,
+
+	"passwordAlgorithm" => "bcrypt",
+	"passwordSaltLength" => 20,
+	"passwordCpuDifficulty" => 16384,
+	"passwordMemDifficulty" => 8,
+	"passwordParallelDifficulty" => 1,
+	"passwordKeyLength" => 32,
+
+	"passwordRounds" => 10
+
+	"activationCodeLength" => 20,
+	"sendMailAddress" => "noreply@localhost",
+	"autologoutTime" => 50000,
+	"maxLoginAttempts" => 5,
+	"loginBlockTime" => 3600,
+	"secureSessions" => false
+);
+
+
+$config = new Config($configArray);
