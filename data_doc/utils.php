dc0db3eb537735e271cd56c825dde7e8232bdd88
Seoester <seoester@googlemail.com>
2013-06-21 00:06:49 +0200

    Added upper case characters to the genCode function

--- data_doc/utils.php
+++ data_doc/utils.php
@@ -16,7 +16,7 @@
 //     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 function genCode($charNum) {
-	$letters = array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+	$letters = array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
 	$code = "";
 	
 	for ($i = 0; $i < $charNum; $i++) {
